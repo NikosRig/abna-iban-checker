92ddcdce970e0950d5cf24060b6812f093cedcef
NikosRig <nikosrigas95@gmail.com>
2022-08-21 22:23:42 +0300

    mockito added as a dependency & public removed from interface

--- src/main/java/nl/abna/ibanchecker/domain/gateway/IbanCheckerGatewayInterface.java
+++ src/main/java/nl/abna/ibanchecker/domain/gateway/IbanCheckerGatewayInterface.java
@@ -3,5 +3,5 @@ package nl.abna.ibanchecker.domain.gateway;
 import nl.abna.ibanchecker.domain.gateway.exceptions.BadResponseException;
 
 public interface IbanCheckerGatewayInterface {
-    public CheckIbanResponse checkIban(final String iban, final String name) throws BadResponseException;
+    CheckIbanResponse checkIban(final String iban, final String name) throws BadResponseException;
 }
