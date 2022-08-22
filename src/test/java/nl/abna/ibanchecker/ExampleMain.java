b259fac5501b23c3a1606db39e1eae83888fd5c9
NikosRig <nikosrigas95@gmail.com>
2022-08-22 22:31:57 +0300

    example.main updated

--- src/test/java/nl/abna/ibanchecker/ExampleMain.java
+++ src/test/java/nl/abna/ibanchecker/ExampleMain.java
@@ -1,6 +1,8 @@
 package nl.abna.ibanchecker;
 
+import nl.abna.ibanchecker.domain.gateway.IbanCheckResponse;
 import nl.abna.ibanchecker.domain.gateway.IbanCheckerClientInterface;
+import org.json.JSONObject;
 
 public class ExampleMain {
     public static void main( String[] args ) throws Exception {
@@ -11,6 +13,15 @@ public class ExampleMain {
                 .withPkcs12("./var/keyStore.p12", "")
                 .build();
 
-        client.checkIban("NL62ABNA9999841479","test name");
+        IbanCheckResponse ibanCheckResponse = client.checkIban("NL62ABNA9999841479","test");
+        JSONObject responsePayload = new JSONObject();
+        responsePayload.put("iban_valid", ibanCheckResponse.isIbanValid);
+        responsePayload.put("name_matched", ibanCheckResponse.isNameMatched);
+        responsePayload.put("foreign_account", ibanCheckResponse.isForeignAccount);
+        responsePayload.put("account_active", ibanCheckResponse.isAccountActive);
+        responsePayload.put("account_country_code", ibanCheckResponse.accountCountryCode);
+        responsePayload.put("total_account_holders", ibanCheckResponse.totalAccountHolders);
+
+        System.out.println(responsePayload);
     }
 }
